2ce5824cabdcf99f404626c08391377e4eced852
Dan White <dan@whiteaudio.com>
2012-09-08 22:55:07 -0500

    Add processor header

---
+++ ns430.h
@@ -0,0 +1,13 @@
+#if !defined(NS430)
+#define NS430
+
+#include <signal.h>
+
+typedef unsigned char uint8_t;
+typedef signed char int8_t;
+typedef unsigned int uint16_t;
+typedef signed int int16_t;
+
+typedef __attribute__((aligned (2))) uint8_t str_t;
+
+#endif
