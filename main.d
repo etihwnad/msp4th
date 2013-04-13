32cc4788a96f0df221d5f45311a1c27be47686ec
Dan White <dan@whiteaudio.com>
2013-04-13 12:52:42 -0500

    add generated files for reference

---
+++ main.d
@@ -0,0 +1,3 @@
+main.o: main.c ns430.h ns430-atoi.h \
+ /usr/lib/gcc/msp430/4.5.3/../../../../msp430/include/iomacros.h \
+ ns430-uart.h msp4th.h
---
+++ ns430-uart.d
@@ -0,0 +1,3 @@
+ns430-uart.o: ns430-uart.c ns430-atoi.h \
+ /usr/lib/gcc/msp430/4.5.3/../../../../msp430/include/iomacros.h \
+ ns430-uart.h ns430.h
